b50140148c308ddc8b7b8859b6cb38d01119b6ab
LuukDobbelaar <l.dobbelaar@student.maastrichtuniversity.nl>
2024-12-01 20:39:15 +0100

    Update testing.py
    added methods for reconstruction methods

--- testing.py
+++ testing.py
@@ -88,7 +88,7 @@ import numpy as np
 
 #ball pivoting
 
-pcd = o3d.io.read_point_cloud("dragon.ply")
+#pcd = o3d.io.read_point_cloud("dragon.ply")
 
 #Techniques to reduce nr of points used.
 # downsample_factor = 0.5  # Keep 10% of the original points (adjust as needed)
@@ -97,10 +97,70 @@ pcd = o3d.io.read_point_cloud("dragon.ply")
 # #voxel_size = 0.01  # Larger voxel size for faster processing (adjust as needed)
 # #pcd = pcd.voxel_down_sample(voxel_size=voxel_size)
 
-pcd.estimate_normals()
-radii = [0.075, 0.125, 0.225]
-print("radii: ", radii)
-mesh_bpa = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(pcd, o3d.utility.DoubleVector(radii))
-mesh_bpa.compute_vertex_normals()
-mesh_bpa.paint_uniform_color([237 / 255, 202 / 255, 29 / 255])
-o3d.visualization.draw_geometries([mesh_bpa], mesh_show_back_face=True)
+# pcd.estimate_normals()
+# radii = [0.075, 0.125, 0.225]
+# print("radii: ", radii)
+# mesh_bpa = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(pcd, o3d.utility.DoubleVector(radii))
+# mesh_bpa.compute_vertex_normals()
+# mesh_bpa.paint_uniform_color([237 / 255, 202 / 255, 29 / 255])
+# o3d.visualization.draw_geometries([mesh_bpa], mesh_show_back_face=True)
+
+def poisson_reconstruction(input_file, output_file, depth):
+    pcd = o3d.io.read_point_cloud(input_file)
+    pcd.estimate_normals()
+    mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(pcd, depth=depth)
+
+    # Optionally clean the mesh by removing low-density vertices
+    vertices_to_remove = densities < np.quantile(densities, 0.01)
+    mesh.remove_vertices_by_mask(vertices_to_remove)
+
+    num_vertices = len(mesh.vertices)
+    print(f"Number of vertices in the mesh: {num_vertices}")
+    o3d.io.write_triangle_mesh(output_file, mesh)
+    mesh.paint_uniform_color([237 / 255, 202 / 255, 29 / 255])
+    o3d.visualization.draw_geometries([mesh], mesh_show_back_face=True)
+
+def alpha_shape_reconstruction(input_file, output_file, alpha):
+
+    print("alpha= ", alpha)
+    pcd = o3d.io.read_point_cloud(input_file)
+    mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_alpha_shape(pcd, alpha)
+    mesh.compute_vertex_normals()
+    o3d.io.write_triangle_mesh(output_file, mesh)
+    mesh.paint_uniform_color([237 / 255, 202 / 255, 29 / 255])
+    o3d.visualization.draw_geometries([mesh], mesh_show_back_face=True)
+
+
+def ball_pivoting_reconstruction(input_file, output_file, radii):
+    #Techniques to reduce nr of points used.
+    # downsample_factor = 0.5  # Keep 10% of the original points (adjust as needed)
+    # pcd = pcd.random_down_sample(downsample_factor)
+
+    # #voxel_size = 0.01  # Larger voxel size for faster processing (adjust as needed)
+    # #pcd = pcd.voxel_down_sample(voxel_size=voxel_size)
+
+    print("radii= ", radii)
+    pcd = o3d.io.read_point_cloud(input_file)
+    pcd.estimate_normals()
+    mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(pcd, o3d.utility.DoubleVector(radii))
+    mesh.compute_vertex_normals()
+    o3d.io.write_triangle_mesh(output_file, mesh)
+    mesh.paint_uniform_color([237 / 255, 202 / 255, 29 / 255])
+    o3d.visualization.draw_geometries([mesh], mesh_show_back_face=True)
+
+def main():
+    input_file = "dragon.ply"
+    depth = 12 #optimal for poisson for dragon
+    alpha = 3.5 #optimal for alpha for dragon
+    radii = [0.075, 0.125, 0.225] #good setting for ball pivoting for dragon
+    poisson_output = "mesh_poisson.ply"
+    alpha_output = "mesh_poisson.ply"
+    ballpiv_output = "mesh_poisson.ply"
+    
+    poisson_reconstruction(input_file, poisson_output, depth)
+    alpha_shape_reconstruction(input_file, alpha_output, alpha)
+    ball_pivoting_reconstruction(input_file, ballpiv_output, radii)
+    #can also add output files to other methods if necessary.
+
+if __name__ == "__main__":
+    main()
